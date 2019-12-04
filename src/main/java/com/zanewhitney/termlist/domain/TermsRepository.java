dbe0fae05f1a8140f6705f0d6d9830b6357a4c8f
Zane Whitney <zane.whitney@gmail.com>
2019-12-04 14:56:41 -0500

    Make database queries pageable.

--- src/main/java/com/zanewhitney/termlist/domain/TermsRepository.java
+++ src/main/java/com/zanewhitney/termlist/domain/TermsRepository.java
@@ -1,17 +1,20 @@
 package com.zanewhitney.termlist.domain;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 import java.util.UUID;
 
 @Repository("termsRepository")
 public interface TermsRepository extends JpaRepository<Term, String> {
-    @Query("SELECT t FROM Term t WHERE t.title like ?1%")
-    List<Term> findTermsBySearchQueryLike(String searchQuery);
+    @Query("SELECT t FROM Term t WHERE t.title like ?1")
+    List<Term> findTopTenTermsBySearchQueryLike(String searchQuery);
 
-    @Query("SELECT t FROM Term t WHERE t.id = ?1%")
-    Term getTermById(UUID id);
+    @Query("SELECT t FROM Term t WHERE t.title like ?1")
+    List<Term> findBySearchQueryLike(String searchQuery, Pageable pageable);
+
+    @Query("SELECT t FROM Term t WHERE t.id = ?1")
+    Term getTermById(String id);
 }
